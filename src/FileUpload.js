4a6035d802b776e7c1053c3ab0adc1f93f8c6b56
Natasha Tan <apple@eduroam-int-dhcp-97-29-240.ucl.ac.uk>
2022-03-08 18:00:10 +0000

    fix upload

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,59 +1,22 @@
 // I watched this tutorial to create the source code of this File Upload Screen: https://www.youtube.com/watch?v=0TTa5Ulmgds&ab_channel=ArjunYonjan%2FCodeTravel
 
 
-import { React, useState, useContext, useRef, useEffect } from 'react';
+import { React, useState, useRef, useEffect } from 'react';
 import axios from 'axios';
-import AuCon from "./AuProv";
 
 const UPLOAD_URL = 'http://localhost:3030/image/upload';
 
-// class FileUpload extends Component {
-//     state = {file:null}
-
-//     handleFile(e){
-//         let file = this.state.file
-//         let formdata = new FormData()
-//         formdata.append('image', file)
-//         formdata.append('name', 'appended')
-//     }
-
-//     handleUpload(e){
-//         axios.post(UPLOAD_URL, JSON.stringify({File}))
-//     }
-
-//     render () {
-//         return (
-//             <div className="App">
-//                 <h1>Congratulations, You Are Logged In!</h1>
-
-//                 <form>
-//                     <label>Please Upload Image Here</label>
-//                     <div>
-//                         <input  type='file' onChange={(e) => this.handleFile(e)}/>
-//                     </div>
-//                     <button onClick={(e)=>this.handleUpload(e)}>Upload File</button>
-//                 </form>
-//             </div>
-//         );
-//     }
-
-// }
-
-// export default FileUpload;
-
-
 const FileUpload = () => {
 
-    const { setAuth } = useContext(AuCon); 
     const errorRef = useRef();
 
-    const [fileName, setFileName] = useState('');
+    const [file, setFile] = useState();
     const [errorMessage, setErrorMessage] = useState('');
     const [success, setSuccess] = useState(false);
 
     useEffect(() => {
         setErrorMessage('');
-    }, [fileName])
+    }, [file])
 
     const Upload = async (e) => {
 
@@ -61,12 +24,18 @@ const FileUpload = () => {
 
         try {
 
+            console.log("look here", file)
+
             let formdata = new FormData()
-            formdata.append('image', fileName)
+            formdata.append('img_data', file)
             formdata.append('name', 'appended')
 
-            await axios.post(UPLOAD_URL, JSON.stringify({ filename: fileName}), {headers: { 'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYXR0YWNrZXIiLCJleHAiOjE2NzgwNjYzNTAsImlhdCI6MTY0NjUzMDM1MCwiaXNzIjoibGVha3ktaW1hZ2UifQ.JBmQ5EFQAEYvQ89tdGT8gO31OqQftGxnYr_RSpnzi18' }});
+            const { data } = await axios.post(UPLOAD_URL, formdata, {
+                withCredentials: true,
+                headers: {'content-type': 'multipart/form-data'}
+            })
 
+            console.log(data)
             setSuccess(true);
             
         } catch (e) {
@@ -91,7 +60,7 @@ const FileUpload = () => {
                         <label>Please Upload Image Here</label>
                         <input
                             type='file' 
-                            onChange={(e) => setFileName(e.target.value)}
+                            onChange={(e) => setFile(e.target.files[0])}
                         />
                         <button>Upload File</button>
                     </form>
